08decd102913427d5b191e2c5a43f244285a5a89
igorskyflyer <igor.dvlpr@gmail.com>
2021-06-22 23:54:24 +0200

    Add Intro.java to AppShare

---
+++ AppShare/app/src/main/java/org/neovisio/appshare/Intro.java
@@ -0,0 +1,15 @@
+package org.neovisio.appshare;
+
+import android.os.*;
+import android.support.v7.app.*;
+import android.support.v7.widget.*;
+import android.view.*;
+import android.widget.*;
+
+public class Intro extends AppCompatActivity {
+ @Override
+ protected void onCreate(Bundle savedInstanceState) {
+  super.onCreate(savedInstanceState);
+  setContentView(R.layout.intro);
+ }
+}
