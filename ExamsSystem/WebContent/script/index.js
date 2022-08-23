ef8e0f0d856338cda79d980cce3ecbbb98a1ff6f
igorskyflyer <igor.dvlpr@gmail.com>
2022-08-23 23:47:12 +0200

    Add index.js

---
+++ ExamsSystem/WebContent/script/index.js
@@ -0,0 +1,25 @@
+/**
+ * @author Maja Devic
+ */
+
+function onRegClick() {
+
+	window.location.replace("registration.jsp");
+
+}
+
+function LogOutClick() {
+
+	window.location.replace("logout.jsp");
+
+}
+
+function onAdminLogin(){
+	window.location.replace("admin_login.jsp");
+
+}
+
+function toMainPage(){
+	
+	window.location.replace("student_prikaz.jsp");
+}
