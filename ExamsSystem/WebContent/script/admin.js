175f591a6e1ec38b4786e2ed8fc3cde78b52bcb4
igorskyflyer <igor.dvlpr@gmail.com>
2022-08-23 23:47:11 +0200

    Add admin.js

---
+++ ExamsSystem/WebContent/script/admin.js
@@ -0,0 +1,27 @@
+/**
+ * @author Maja Devic
+ */
+
+function onShowRelsClick(){
+	window.location.replace("admin_show_test_rel.jsp");
+}
+
+function onShowAllStudents(){
+	window.location.replace("admin_all_students.jsp");
+}
+
+function toMainPanel(){
+	window.location.replace("admin_view.jsp");
+}
+
+function onShowAllExams(){
+	window.location.replace("admin_all_exams.jsp");
+}
+
+function onShowPassedExams(){
+	window.location.replace("admin_passed_exams.jsp");
+}
+
+function onEdit(){
+	window.location.replace("admin_edit.jsp");
+}
